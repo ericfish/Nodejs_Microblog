c7b14bf1ffe74ff9e550b9f33b108593220cf594
ericfish <ericfish@gmail.com>
2013-06-09 14:27:35 +0800

    fix Express 3 flash & app.dynamicHelpers not exist issue

--- app.js
+++ app.js
@@ -6,46 +6,59 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
-
-var settings = require('./settings');
-var MongoStore = require('connect-mongo')(express);
-
+  , path = require('path')
+  , settings = require('./settings')
+  , MongoStore = require('connect-mongo')(express)
+  , flash = require('connect-flash');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Configuration
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.configure(function(){
+  // set environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
 
-app.use(express.cookieParser());
-app.use(express.session({
-  secret: settings.cookieSecret,
-  store: new MongoStore({
-    db: settings.db
-  })
-}));
+  //加载子模板文件
+  app.use(flash());
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
 
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+  //session related
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: settings.cookieSecret,
+    store: new MongoStore({
+      db: settings.db
+    })
+  }));
+
+  app.use(function (req, res, next) {
+      res.locals.error = req.flash('error');
+      res.locals.success = req.flash('success');
+      res.locals.user = req.session.user;
+      next();
+  });
+
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
+// production only
 if ('production' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 routes(app);
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
