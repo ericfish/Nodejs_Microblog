696da8f214a3d898daddfc072c659c69edef8253
ericfish <ericfish@gmail.com>
2013-06-12 22:57:08 +0800

    add authentication function of mengodb

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 
 ###Introduction
 
-这个repo是[《Node.js开发指南》](http://www.ituring.com.cn/book/1049)的书中源码兼容express.js 3.x的版本，更新了一些相关代码的支持。
+这个repo是[《Node.js开发指南》](http://www.ituring.com.cn/book/1049)的书中源码兼容express.js 3.x的版本，更新了相关代码的支持。
+另外实现了对mengodb authentication的支持。
 
 ###Caution
 
--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 var mongodb = require('./db');
+var util = require('../libs/util');
 
 function Post(username, post, time) {
   this.user = username;
@@ -22,18 +23,22 @@ Post.prototype.save = function save(callback) {
     if (err) {
       return callback(err);
     }
-    // 讀取 posts 集合
-    db.collection('posts', function(err, collection) {
-      if (err) {
-        mongodb.close();
-        return callback(err);
-      }
-      // 爲 user 屬性添加索引
-      collection.ensureIndex('user');
-      // 寫入 post 文檔
-      collection.insert(post, {safe: true}, function(err, post) {
-        mongodb.close();
-        callback(err, post);
+
+    // 验证用户名和密码
+    util.authenticateAndGo(db, function() {
+      // 讀取 posts 集合
+      db.collection('posts', function(err, collection) {
+        if (err) {
+          mongodb.close();
+          return callback(err);
+        }
+        // 爲 user 屬性添加索引
+        collection.ensureIndex('user');
+        // 寫入 post 文檔
+        collection.insert(post, {safe: true}, function(err, post) {
+          mongodb.close();
+          callback(err, post);
+        });
       });
     });
   });
@@ -44,29 +49,33 @@ Post.get = function get(username, callback) {
     if (err) {
       return callback(err);
     }
-    // 讀取 posts 集合
-    db.collection('posts', function(err, collection) {
-      if (err) {
-        mongodb.close();
-        return callback(err);
-      }
-      // 查找 user 屬性爲 username 的文檔，如果 username 是 null 則匹配全部
-      var query = {};
-      if (username) {
-        query.user = username;
-      }
-      collection.find(query).sort({time: -1}).toArray(function(err, docs) {
-        mongodb.close();
+
+    // 验证用户名和密码
+    util.authenticateAndGo(db, function() {
+      // 讀取 posts 集合
+      db.collection('posts', function(err, collection) {
         if (err) {
-          callback(err, null);
+          mongodb.close();
+          return callback(err);
         }
-        // 封裝 posts 爲 Post 對象
-        var posts = [];
-        docs.forEach(function(doc, index) {
-          var post = new Post(doc.user, doc.post, doc.time);
-          posts.push(post);
+        // 查找 user 屬性爲 username 的文檔，如果 username 是 null 則匹配全部
+        var query = {};
+        if (username) {
+          query.user = username;
+        }
+        collection.find(query).sort({time: -1}).toArray(function(err, docs) {
+          mongodb.close();
+          if (err) {
+            callback(err, null);
+          }
+          // 封裝 posts 爲 Post 對象
+          var posts = [];
+          docs.forEach(function(doc, index) {
+            var post = new Post(doc.user, doc.post, doc.time);
+            posts.push(post);
+          });
+          callback(null, posts);
         });
-        callback(null, posts);
       });
     });
   });
